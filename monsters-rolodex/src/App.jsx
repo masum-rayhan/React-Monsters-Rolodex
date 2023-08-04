565569c1ce95752040a69a0e2146b78e238497d4
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-08-04 18:20:16 +0600

    storing original data

--- monsters-rolodex/src/App.jsx
+++ monsters-rolodex/src/App.jsx
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
   }
 
@@ -23,6 +24,9 @@ class App extends Component {
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
     return (
       <>
         <div>
@@ -31,16 +35,13 @@ class App extends Component {
             type="search"
             placeholder="search monsters"
             onChange={(event) => {
-              const searchString = event.target.value.toLocaleLowerCase();
-              const filteredMonsters = this.state.monsters.filter((monster) => {
-                return monster.name.toLocaleLowerCase().includes(searchString);
-              });
+              const searchField = event.target.value.toLocaleLowerCase();
               this.setState(() => {
-                return { monsters: filteredMonsters };
+                return { searchField };
               });
             }}
           />
-          {this.state.monsters.map((monster) => {
+          {filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
