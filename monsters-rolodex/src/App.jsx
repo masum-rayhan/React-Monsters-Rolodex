70e08891bc6a711771a5acad655db308fe07918e
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-08-04 18:30:07 +0600

    optimizations

--- monsters-rolodex/src/App.jsx
+++ monsters-rolodex/src/App.jsx
@@ -9,7 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: "",
     };
   }
 
@@ -23,9 +23,20 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
     return (
       <>
@@ -34,12 +45,7 @@ class App extends Component {
             className="search-box"
             type="search"
             placeholder="search monsters"
-            onChange={(event) => {
-              const searchField = event.target.value.toLocaleLowerCase();
-              this.setState(() => {
-                return { searchField };
-              });
-            }}
+            onChange={onSearchChange}
           />
           {filteredMonsters.map((monster) => {
             return (
